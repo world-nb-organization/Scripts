56e4860c2de5e6d06919a028ed30b837d6b176b4
aac6fef <89452382+aac6fef@users.noreply.github.com>
2022-05-31 18:20:40 +0800

    Update install.sh

--- Kde-mountiso/install.sh
+++ Kde-mountiso/install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/bash
 mkdir ~/.local/share/Scripts -p
+mkdir ~/.local/share/kio/servicemenus/ -p
 wget https://raw.githubusercontent.com/world-nb-organization/Scripts/main/Kde-mountiso/mountiso.sh -O ~/.local/share/Scripts/mountiso.sh
 wget https://raw.githubusercontent.com/world-nb-organization/Scripts/main/Kde-mountiso/mountiso.desktop -O ~/.local/share/kio/servicemenus/mountiso.desktop
