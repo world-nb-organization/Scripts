57689f080f6bd41b01009cea6425051b707ccd43
aac6fef <aac6fef@gmail.com>
2022-05-31 18:59:13 +0800

    wdnmd

--- Kde-mountiso/install.sh
+++ Kde-mountiso/install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/bash
+
 install() {
-    mkdir /usr/share/Scripts" -p
-    wget https://raw.githubusercontent.com/world-nb-organization/Scripts/main/Kde-mountiso/mountiso.sh -O "/usr/share/Scripts/mountiso.sh"
+    sudo mkdir "/usr/share/Scripts" -p
+    sudo wget https://raw.githubusercontent.com/world-nb-organization/Scripts/main/Kde-mountiso/mountiso.sh -O "/usr/share/Scripts/mountiso.sh"
     wget https://raw.githubusercontent.com/world-nb-organization/Scripts/main/Kde-mountiso/mountiso.desktop -O "$ServiceMenuDir/mountiso.desktop"
-    chmod 777 -R "/usr/share/Scripts" "$ServiceMenuDir"
+    sudo chmod 777 -R "/usr/share/Scripts" "$ServiceMenuDir"
 }
 
 if [ "${USER}" = root ]; then
-    ServiceMenuDir=/usr/share/kio/servicemenus
+    ServiceMenuDir="/usr/share/kio/servicemenus"
     install
 else
-    ServiceMenuDir="$HOME/.local/share/kio/servicemenus/"
+    ServiceMenuDir="$HOME/.local/share/kio/servicemenus"
     install
 fi
